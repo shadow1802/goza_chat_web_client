48a58e88190beac811ec2b2b2621d0f5f8e216c4
DARKNESS IT WEB <darkness@okvip.com>
2023-12-27 21:24:02 +0700

    show login

--- app/(auth)/login/login.container.tsx
+++ app/(auth)/login/login.container.tsx
@@ -135,12 +135,59 @@ export default function LoginContainer({ invition }: Props) {
         }
     }
 
-    const onLogin = async () => {
-        
+    const login = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const form = event.currentTarget
+        const elements = form.elements as typeof form.elements & {
+            username: { value: string },
+            password: { value: string },
+        }
+
+        try {
+            const { username, password } = elements
+            console.log(username, password)
+            const res = await fetch(process.env.NEXT_PUBLIC_API + "/user/login", {
+                headers: { "Content-Type": "application/json" },
+                method: "POST",
+                body: JSON.stringify({ username: username.value, password: password.value })
+            })
+
+            const { status, data, message } = await res.json()
+
+            if (status === 200) {
+                setCookie("auth", JSON.stringify({
+                    token: data.token, user: {
+                        _id: data.user._id, username: data.user.username, role: data.user.role, bio: data.user.bio
+                    }
+                }))
+                toast({
+                    title: message,
+                    duration: 2000,
+                    description: <p className='text-green-500 font-semibold'>{message}</p>,
+                })
+                setLoading(true)
+
+                if (invition) {
+                    router.push(`/invition/${invition}`)
+                } else router.push("/")
+            } else {
+                toast({
+                    duration: 2000,
+                    title: message,
+                    description: <p className='text-red-500 font-semibold'>{message}</p>,
+                })
+            }
+        } catch (error) {
+
+        }
+    }
+
+    const loginWithPhone = async () => {
+
         const client = nanoid()
         socket.emit("pre_verify_login", client)
 
-        setOpenLoginVerify(JSON.stringify({client}))
+        setOpenLoginVerify(JSON.stringify({ client }))
 
         /*
         try {
@@ -220,8 +267,8 @@ export default function LoginContainer({ invition }: Props) {
             </DialogContent>
         </Dialog>
 
-        <form ref={form} className="flex flex-col justify-center items-center">
-            <div className="bg-gray-300 rounded-full drop-shadow-lg shadow-lg shadow-white w-44 h-44">
+        <form ref={form} onSubmit={login} className="bg-white w-[450px] shadow-sm shadow-white rounded-lg p-8 flex flex-col justify-center items-center">
+            <div className='bg-gray-300 rounded-full drop-shadow-lg shadow-lg shadow-white'>
                 <img src="/images/logo.png" className="w-44 h-44" alt="" />
             </div>
 
@@ -229,14 +276,22 @@ export default function LoginContainer({ invition }: Props) {
 
                 <Title size={26} />
                 <h2 className="text-2xl font-semibold text-gray-600">Đăng nhập</h2>
+                <p className='text-center text-gray-600 font-semibold'>Vui lòng nhập đúng tài khoản và mật khẩu của bạn để đăng nhập</p>
 
-                <p className="max-w-[333px] text-center font-semibold text-gray-500">Đăng nhập vào GOZA bằng cách quét QR với thiết bị của bạn</p>
+                <Scanner name='username' icon={<CiUser />} label="Tên tài khoản *" placeholder="Tên tài khoản phải lớn hơn 6 ký tự" />
+                <Scanner name='password' type='password' icon={<CiLock />} placeholder="Mật khẩu phải lớn hơn 6 ký tự" label="Mật khẩu *" />
 
-                <button type="button" onClick={onLogin} className='text-white w-full hover:bg-sky-400 font-semibold duration-150 shadow-md shadow-white rounded-lg py-2 px-6 bg-sky-500'>
-                    Đăng nhập bằng điện thoại
-                </button>
+                <div className="flex justify-end">
+                    <p className='text-gray-500 font-semibold text-sm'>Chưa có tài khoản? <Link className='text-sky-500' href={"/register"}>Đăng ký ngay</Link></p>
+                </div>
+
+                <button type="submit" className='text-white w-full hover:bg-sky-400 font-semibold duration-150 shadow-md shadow-white rounded-lg py-2 bg-sky-500'>Đăng Nhập</button>
             </div>
         </form>
+
+        <button type="button" onClick={loginWithPhone} className='text-white w-full font-semibold duration-150 shadow-md shadow-white rounded-lg py-2 px-6 from-cyan-500 to-blue-500 bg-gradient-to-r'>
+            Đăng nhập bằng điện thoại
+        </button>
         <div className="my-4 w-full flex space-x-2 items-center justify-center">
             <div className="h-[2px] w-[120px] bg-gray-300" />
             <p className="font-semibold text-gray-600 text-sm">Hoặc đăng nhập với</p>
